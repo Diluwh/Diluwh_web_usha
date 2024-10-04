5252683e1fb797212256cfa219c58d52f1e31317
Dilusha <104965983+Diluwh@users.noreply.github.com>
2024-10-05 00:01:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let slideIndex = 0;
 showSlides();
 
+document.querySelector('.dropbtn').addEventListener('click', function () {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+});
+
+
 function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
